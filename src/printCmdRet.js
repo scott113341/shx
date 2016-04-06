6f2219de56645ed8c59e4164da11f431c5aacc1f
Scott Hardy <scott.the.hardy@gmail.com>
2016-04-05 20:39:31 -0700

    Simplified printCmdRet

--- src/printCmdRet.js
+++ src/printCmdRet.js
@@ -2,16 +2,13 @@
 // Invoke this *REGARDLESS* of what the command returns, it will figure it out.
 export const printCmdRet = (ret) => {
   if (!ret) return;
-
-  // This is way to complicated. It should get much easier once shelljs/shelljs#356 is fixed.
-  if (ret.stdout && ret.stderr) {
-    if (ret.stdout) console.log(ret.stdout);
-    if (ret.stderr) console.error(ret.stderr);
-  } else if (ret.output) {
-    console.log(ret.output);
-  } else if (Array.isArray(ret)) {
-    console.log(ret.join('\n'));
-  } else {
-    console.log(ret);
-  }
+  if (ret.stdout) _write('stdout', ret.stdout);
+  if (ret.stderr) _write('stderr', ret.stderr);
 };
+
+
+function _write(std, output) {
+  const stds = ['stderr', 'stdout'];
+  if (stds.indexOf(std) === -1) throw new Error(`Cannot write to '${std}'.`);
+  process[std].write(output);
+}
